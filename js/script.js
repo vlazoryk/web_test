a6d3c356353ea4272fbbd1d6b2a48572f8068eaa
vlazoryk <v.lazoryk@chnu.edu.ua>
2022-09-02 10:12:49 +0300

    js test3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@ function AddToDiv() {
     var eldiv = document.getElementById('id_div');
     eldiv.innerHTML = '<p> Add to div text !</p>'
 }
+
+function DW(){
+    document.writeln(" any test");
+    console.log ("Не вдалося відкрити вказане посилання");
+
+}
 function test() {
     alert(" Test");
+    let isBoss = confirm ("Ти тут головний?");
+alert (isBoss); // true, якщо натиснута OК
+
+let age = prompt ('Скільки тобі років?', 100);
+alert ("Тобі $ {age} років!"); // Тобі 100 років! (оператор)
+
+
 
 }
